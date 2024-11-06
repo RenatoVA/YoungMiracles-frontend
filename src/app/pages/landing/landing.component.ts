4228cc0bac5e092cd895debe8544ef56b276f488
RenatoVA <renato.vivas2001@gmail.com>
2024-11-06 09:33:25 -0500

    HU-41 Landing-page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import {CommonModule} from '@angular/common';
-
+import { trigger, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-landing',
   imports: [FooterComponent,CommonModule],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
-  standalone: true
+  standalone: true,
+
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit, OnDestroy {
   menuItems = [
     { label: 'NOSOTROS', link: '#' },
     { label: 'SERVICIOS', link: '#' },
-    { label: 'CREAR UNA CUENTA', link: '#' },
     { label: 'CONTACTANOS', link: '#' }
   ];
 
@@ -23,4 +23,61 @@ export class LandingComponent {
     { icon: 'bar_chart', label: 'Resultados garantizados' },
     { icon: 'public', label: 'Cobertura nacional' }
   ];
+  slides = [
+    {
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_1r0idMhAZfZazbxI5BVh_xdm7mdyxJLTBQ&s',
+      alt: 'Healthcare professional interacting with patient'
+    },
+    {
+      image: 'https://dulcehogar.pe/wp-content/uploads/2021/03/casa-de-reposo-lima-peru-adultos-mayores-blog-1.jpg',
+      alt: 'Caregiver assisting person in wheelchair'
+    },
+    {
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn4RRsj907QT0b0IWUeOri6wCw2VeMsVXsmQ&s',
+      alt: 'Therapeutic hand massage'
+    }
+  ];
+
+  currentSlide = 0;
+  private autoSlideInterval: any;
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  private startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  private stopAutoSlide() {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+    }
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  previousSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
+
+  onSlideHover(isHovering: boolean) {
+    if (isHovering) {
+      this.stopAutoSlide();
+    } else {
+      this.startAutoSlide();
+    }
+  }
 }
