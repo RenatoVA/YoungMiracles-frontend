97d9163e03022a2b2e4c4f17a19b19f29ec9585a
RenatoVA <renato.vivas2001@gmail.com>
2024-11-21 15:24:53 -0500

    asdasd

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'hollow-maurise-shifer-e06839fb.koyeb.app/api/v1'
+    apiUrl: 'http://localhost:8080/api/v1'
+    //paypalReturnUrl: 'http://localhost:4200/customer/cart'
 
 };
